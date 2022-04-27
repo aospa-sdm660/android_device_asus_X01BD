cb0d19c2c1d5fa98a7d35aa9cbae37767fa49034
Michael Bestas <mkbestas@lineageos.org>
2022-04-27 22:37:53 +0530

    X01BD: Import extract files scripts
    Change-Id: Iafb6a03bf06f8ef4e1486b689436a3464e07e81f

---
+++ extract-files.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+    case "${1}" in
+
+    # Fix jar path
+    product/etc/permissions/qti_fingerprint_interface.xml)
+        sed -i 's|/system/framework/|/system/product/framework/|g' "${2}"
+        ;;
+
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=X01BD
+export DEVICE_COMMON=sdm660-common
+export VENDOR=asus
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
